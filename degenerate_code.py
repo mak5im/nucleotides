da2b87516ea846a7274fe1200248d4994b616165
mak5im <jestinmaxim@gmail.com>
2016-05-03 07:42:55 -0400

    Update degenerate_code.py

--- degenerate_code.py
+++ degenerate_code.py
@@ -1,25 +1,33 @@
 # produce all possible oligos of a primer sequence with degenerate nucleotides 
 
 from itertools import product # computes cartesian product of input iterables
+import os
 
-seq = raw_input('Enter degenerate primer sequence: ')
+while choice == 'Y':
+        seq = raw_input('Enter degenerate primer sequence: ').upper()
 
-degnuc = {'R':['A','G'], 'Y':['C','T'], 'S':['G','C'],
+        degnuc = {'R':['A','G'], 'Y':['C','T'], 'S':['G','C'],
         'W':['A','T'], 'K':['G','T'], 'M':['A','C'],
         'B':['C','G','T'], 'N':['A','T','C','G']}
 
-def multi_replace(seq, degnuc):
-    indexes, replacements = zip(*[(i, degnuc[c]) for i, c in enumerate(seq) if c in degnuc])
+        def multi_replace(seq, degnuc):
+                indexes, replacements = zip(*[(i, degnuc[c]) for i, c in enumerate(seq) if c in degnuc])
     
-    l = list(seq) # turn input into mutable object (changeable)
+                l = list(seq) # turn input into mutable object (changeable)
     
-    # iterate over cartesian product of all replacement tuples
+                 # iterate over cartesian product of all replacement tuples
     
-    for p in product(*replacements):
-        for index, replacement in zip(indexes, p):
-            l[index] = replacement
-        yield ''.join(l)
+        for p in product(*replacements):
+                for index, replacement in zip(indexes, p): 
+                        l[index] = replacement
+                yield ''.join(l)
 
-for perm in multi_replace(seq, degnuc):
-    print perm
+        for perm in multi_replace(seq, degnuc):
+                print perm
+                
+        choice = raw_input('Enter another sequence? \n(Y/N): ').upper()
+        if choice = 'N':
+                os.system('exit')
+        
+        
    
